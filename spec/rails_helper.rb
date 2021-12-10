4c349547691929135a70c5a5d94199cac8e53486
root <colehulick17@gmail.com>
2021-12-10 21:04:51 +0000

    testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,5 @@
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -10,9 +8,6 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
-require 'devise'
-require_relative 'support/controller_macros'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -65,10 +60,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
 
-  # Add these
-  config.include Devise::Test::ControllerHelpers, :type => :controller
-  config.include FactoryBot::Syntax::Methods
-  config.extend ControllerMacros, :type => :controller
-
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/' # for rspec
 end
